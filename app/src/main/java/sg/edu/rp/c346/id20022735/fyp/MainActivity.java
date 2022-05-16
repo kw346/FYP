28800a8477a84898048861888150fdd9beb96c6a
zanewashere <20045805@myrp.edu.sg>
2022-05-16 15:32:14 +0800

    Updated MainActivity, Added HomePage

--- app/src/main/java/sg/edu/rp/c346/id20022735/fyp/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20022735/fyp/MainActivity.java
@@ -2,13 +2,27 @@ package sg.edu.rp.c346.id20022735.fyp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, HomePage.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
